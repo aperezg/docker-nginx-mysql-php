dfa0223fc8f8913eae13143f06b04456b4189275
Adrian Perez <perezg.adrian@gmail.com>
2016-09-28 23:39:22 +0200

    Configure mysql and create own php7 alpine image

--- src/index.php
+++ src/index.php
@@ -2,7 +2,22 @@
 <body>
 	<h1>Hello Docker!</h1>
 	<?php 
-		echo "Ready to use php";
+		echo "Can use php? ¡yes!";
+	?>
+	<br/>
+	<?php
+
+		$mysqlFlag = "No :(";
+		try {
+			
+			$pdo = new PDO('mysql:host=db;dbname=docker_db','docker','docker');
+			$mysqlFlag = "¡yes!";
+
+		} catch(Exception $e) {
+			var_dump($e);
+		}
+
+		echo "Can use mysql? ".$mysqlFlag;
 	?>
 </body>
 </html>
