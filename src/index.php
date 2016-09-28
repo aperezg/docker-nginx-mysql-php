ceb6d6fba01410526f8355f2b329bcceabccb90b
Idir Ouhab Meskine <ouhab.idir@gmail.com.com>
2016-09-29 01:11:10 +0200

    Create new nginx container

--- src/index.php
+++ src/index.php
@@ -1,8 +1,20 @@
 <html>
 <body>
-	<h1>Hello Docker!</h1>
-	<?php 
-		echo "Ready to use php";
-	?>
+<h1>Hello Docker!</h1>
+<?php
+$servername = "db";
+$username = "docker";
+$password = "docker";
+$dbname = "docker_db";
+echo "Ready to use php<br>";
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    echo "<strong>- With Mysql!</strong>";
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
+
+?>
 </body>
 </html>
